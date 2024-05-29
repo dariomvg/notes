b40714c16982198e7af77e48cd07c2a5fa99bf6f
dariomvg <contact.dario.info@gmail.com>
2024-05-29 19:24:04 -0300

    add url backend

--- src/services/deleteImage.js
+++ src/services/deleteImage.js
@@ -1,5 +1,7 @@
+import { URL_BACKEND } from "../url";
+
 export const deleteImageCloud = async (images) => {
-  const response = await fetch(`${import.meta.env.VITE_URL_BACKEND}/delete-image`, {
+  const response = await fetch(`${URL_BACKEND}/delete-image`, {
     method: "POST",
     headers: {"Content-type": "application/json; charset=UTF-8"},
     body: JSON.stringify(images),
