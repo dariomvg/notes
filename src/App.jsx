76692f49cfbe6e03ba562a9fec4f1cb5b7c9c6bd
dariomvg <contact.dario.info@gmail.com>
2024-05-29 18:33:43 -0300

    change enviroment variable

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const ButtonCreate = React.lazy(() => import('./components/ButtonCreate'));
 export default function App() {
   const { notes, toggle } = useContextNotes();
   const { theme } = useContextTheme();
-
+  
   return (
     <main className="wrapper">
       <Header />
